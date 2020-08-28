7dd75388b6299540ac1b026bff41a58664b594d5
Thomas Sauerer <tsauerer@tsauerer-a01.vmware.com>
2020-08-28 08:58:00 +0200

    Commit

--- Firewall/main.tf
+++ Firewall/main.tf
@@ -4,3 +4,72 @@ provider "nsxt" {
   allow_unverified_ssl = true
   enforcement_point    = "vmc-enforcementpoint"
 }
+
+
+resource "nsxt_policy_gateway_policy" "mgw_policy" {
+  category        = "LocalGatewayRules"
+  display_name    = "default"
+  domain          = "mgw"
+
+  Lifecycle {
+  prevent_destroy = true
+  }
+
+  rule {
+      action                = "ALLOW"
+      destination_groups    = [
+        "/infra/domains/mgw/groups/VCENTER",
+      ]
+      destinations_excluded = false
+      direction             = "IN_OUT"
+      disabled              = false
+      display_name          = "vCenter Inbound"
+      ip_version            = "IPV4_IPV6"
+      logged                = false
+      profiles              = []
+      scope                 = [
+          "/infra/labels/mgw",
+      ]
+      services              = ["/infra/services/HTTPS"]
+      source_groups         = []
+      sources_excluded      = false
+  }
+  rule {
+      action                = "ALLOW"
+      destination_groups    = []
+      destinations_excluded = false
+      direction             = "IN_OUT"
+      disabled              = false
+      display_name          = "ESXi Outbound Rule"
+      ip_version            = "IPV4_IPV6"
+      logged                = false
+      profiles              = []
+      scope                 = [
+          "/infra/labels/mgw",
+      ]
+      services              = []
+      source_groups         = [
+          "/infra/domains/mgw/groups/ESXI",
+      ]
+      sources_excluded      = false
+  }
+  rule {
+      action                = "ALLOW"
+      destination_groups    = []
+      destinations_excluded = false
+      direction             = "IN_OUT"
+      disabled              = false
+      display_name          = "vCenter Outbound Rule"
+      ip_version            = "IPV4_IPV6"
+      logged                = false
+      profiles              = []
+      scope                 = [
+          "/infra/labels/mgw",
+      ]
+      services              = []
+      source_groups         = [
+          "/infra/domains/mgw/groups/VCENTER",
+      ]
+      sources_excluded      = false
+  }
+}
